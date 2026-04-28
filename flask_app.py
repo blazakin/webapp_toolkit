faf704bad44513fbd8ae9ce86d21d443a46f420c
Forrest <felschf05@gmail.com>
2026-04-27 21:56:18 -0700

    feat: initial portfolio page and 404 page

--- flask_app.py
+++ flask_app.py
@@ -24,5 +24,19 @@ def index():
 def task_app():
     return render_template('task_app.html')
 
+@app.route('/Portfolio', methods=['GET'])
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/Dashboards/Ham_Dashboard', methods=['GET'])
+def ham_dashboard():
+    return render_template('/dashboards/ham_dashboard.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
